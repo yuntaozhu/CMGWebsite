1d1d27f3653d22ff59955e4cbe7e972a3804c9ef
Danielle Araez <draraez@up.edu.ph>
2023-08-02 01:44:57 +0800

    chore: edited hero logo decorations

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -47,11 +47,11 @@
         class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
         <a href="/" class="flex items-center">
-            <img src="/logo.webp" class="h-8 mr-3" alt="Flowbite Logo" />
-            <span
+            <img src="/logo.webp" class="h-16 mr-3" alt="Flowbite Logo" />
+            <!-- <span
                 class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
                 >ACSS</span
-            >
+            > -->
         </a>
         <button
             type="button"
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -18,31 +18,37 @@
         >
             <!-- Hero ACSS Logo -->
             <div class="relative">
+                <!-- Gradient Shapes at the Back -->
+
                 <img
                     src="/home/ellipse-1.svg"
                     alt="Gradient Ellipse"
-                    class="center-img hexagon"
+                    class="absolute top-[16%] left-[68%] h-20"
                 />
                 <img
                     src="/home/ellipse-1.svg"
                     alt="Gradient Ellipse"
-                    class="center-img hexagon"
+                    class="absolute bottom-[20%] left-[28%] h-10"
                 />
                 <img
                     src="/home/gradient-hexagon.svg"
                     alt="Gradient Hexagon"
-                    class="center-img hexagon"
+                    class="absolute top-[8%] lg:top-[2%] left-[10%] sm:left-[20%] lg:left-[10%] xl:left-[20%] h-40 lg:h-60"
                 />
                 <img
                     src="/home/gradient-hexagon.svg"
                     alt="Gradient Hexagon"
-                    class="center-img hexagon"
+                    class="absolute bottom-[12%] left-[62%] h-24"
                 />
+
+                <!-- Logo in Glassmorphic -->
+
                 <img
                     src="/home/glassmorphic-hexagon.svg"
                     alt="Glassmorphic Hexagon"
                     class="center-img hexagon"
                 />
+
                 <img
                     src="/home/white-logo.webp"
                     alt="The official seal of The Alliance of Computer Science Students"
@@ -52,6 +58,7 @@
 
             <!-- Hero Caption -->
             <div class="flex flex-col justify-center px-20 py-16">
+                <div class="gradient-circle" />
                 <p
                     class="mb-4 text-4xl font-bold leading-tight md:text-5xl md:leading-snug tracking-wide text-white"
                 >
@@ -86,17 +93,33 @@
     }
 
     .hexagon {
-        width: 400px;
-        height: 400px;
-        margin-top: -200px; /* Half the height */
-        margin-left: -200px; /* Half the width */
+        width: 340px;
+        height: 340px;
+        margin-top: -170px; /* Half the height */
+        margin-left: -170px; /* Half the width */
     }
 
     .logo {
-        width: 260px;
-        height: 260px;
-        margin-top: -140px; /* Half the height */
-        margin-left: -130px; /* Half the width */
+        width: 240px;
+        height: 240px;
+        margin-top: -130px; /* Half the height */
+        margin-left: -120px; /* Half the width */
+    }
+
+    @media (min-width: 1024px) {
+        .hexagon {
+            width: 400px;
+            height: 400px;
+            margin-top: -200px; /* Half the height */
+            margin-left: -200px; /* Half the width */
+        }
+
+        .logo {
+            width: 260px;
+            height: 260px;
+            margin-top: -140px; /* Half the height */
+            margin-left: -130px; /* Half the width */
+        }
     }
 
     .glassmorphic-box {
@@ -111,5 +134,71 @@
             rgba(255, 255, 255, 0) 100%
         );
         backdrop-filter: blur(21px);
+    }
+
+    .gradient-blur-circle {
+        width: 682px;
+        height: 682px;
+        flex-shrink: 0;
+        border-radius: 682px;
+        background: linear-gradient(
+                    to bottom right,
+                    rgba(0, 245, 241, 0.06) 0%,
+                    rgba(46, 55, 146, 0.15) 50%
+                )
+                bottom right / 50% 50% no-repeat,
+            linear-gradient(
+                    to bottom left,
+                    rgba(0, 245, 241, 0.06) 0%,
+                    rgba(46, 55, 146, 0.15) 50%
+                )
+                bottom left / 50% 50% no-repeat,
+            linear-gradient(
+                    to top left,
+                    rgba(0, 245, 241, 0.06) 0%,
+                    rgba(46, 55, 146, 0.15) 50%
+                )
+                top left / 50% 50% no-repeat,
+            linear-gradient(
+                    to top right,
+                    rgba(0, 245, 241, 0.06) 0%,
+                    rgba(46, 55, 146, 0.15) 50%
+                )
+                top right / 50% 50% no-repeat;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        filter: blur(20px);
+    }
+
+    .gradient-blur-circle-2 {
+        width: 659px;
+        height: 659px;
+        flex-shrink: 0;
+        border-radius: 659px;
+        background: linear-gradient(
+                    to bottom right,
+                    rgba(0, 245, 241, 0.08) 0%,
+                    rgba(46, 55, 146, 0.2) 50%
+                )
+                bottom right / 50% 50% no-repeat,
+            linear-gradient(
+                    to bottom left,
+                    rgba(0, 245, 241, 0.08) 0%,
+                    rgba(46, 55, 146, 0.2) 50%
+                )
+                bottom left / 50% 50% no-repeat,
+            linear-gradient(
+                    to top left,
+                    rgba(0, 245, 241, 0.08) 0%,
+                    rgba(46, 55, 146, 0.2) 50%
+                )
+                top left / 50% 50% no-repeat,
+            linear-gradient(
+                    to top right,
+                    rgba(0, 245, 241, 0.08) 0%,
+                    rgba(46, 55, 146, 0.2) 50%
+                )
+                top right / 50% 50% no-repeat;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        filter: blur(40px);
     }
 </style>
