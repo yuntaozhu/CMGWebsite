346bb042621966e88c7870f5d3d9a350674c71d7
Danielle Araez <draraez@up.edu.ph>
2023-08-05 17:48:35 +0800

    chore: Home | Started Incoming Event Section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
     content: ["./src/**/*.{svelte,js,ts}"],
     theme: {
+        screens: {
+            'ss': '425px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        },
         letterSpacing: {
             wide: '.025em',
         },
