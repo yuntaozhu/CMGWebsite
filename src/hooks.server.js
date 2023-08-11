c6afdc430241018a5d9dc840383aa8008f05f94b
kennethtegrado <kategrado@up.edu.ph>
2023-08-11 19:01:51 +0800

    feat: update headers

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,12 +1,11 @@
 const securityHeaders = {
     // "Cross-Origin-Embedder-Policy": "require-corp",
     // "Cross-Origin-Opener-Policy": "same-origin",
-    // "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
-    // "X-Content-Type-Options": "nosniff",
-    // "X-Frame-Options": "SAMEORIGIN",
-    // "X-XSS-Protection": "1; mode=block",
-    // "Pemissions-Policy": "geolocation=(), microphone=(), camera=()",
-    // "Referrer-Policy": "no-referrer, strict-origin-when-cross-origin",
+    "X-Content-Type-Options": "nosniff",
+    "X-Frame-Options": "SAMEORIGIN",
+    "X-XSS-Protection": "1; mode=block",
+    "Pemissions-Policy": "geolocation=(), microphone=(), camera=()",
+    "Referrer-Policy": "no-referrer, strict-origin-when-cross-origin",
 };
 
 export const handle = async ({ event, resolve }) => {
