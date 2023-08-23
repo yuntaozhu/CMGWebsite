393bec03edb6f25cf82166146c7d7f929f7d5527
Kael-Makiling <mrmakiling@up.edu.ph>
2023-08-23 16:15:53 +0800

    ui: Contact Us Improvements

--- src/components/organisms/ContactUs/ContactForm.svelte
+++ src/components/organisms/ContactUs/ContactForm.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { Button, HamburgerMenu } from "$components";
+  import { Button } from "$components";
 
   let string = "",
     buttonVisible = true;
@@ -10,66 +10,63 @@
   }
 </script>
 
-<div class="flex flex-row justify-around items-center">
-  <div class="contact__left flex justify-center grow">
-    <h1>Photo</h1>
+<div class="grid grid-cols-10 pl-20 pr-20 h-screen">
+  <div class="flex justify-center items-center lg:col-span-5 xl:col-span-4">
+    <div class="hidden justify-center xl:justify-end lg:p-10 lg:block xl:block">
+      <img src="/assets/contactUS-photo.png" alt="photo" />
+    </div>
   </div>
-  <div class="contact__right flex justify-center grow">
-    <div class="contact__right-container flex flex-col gap-3 self-center">
-      <h4 class="contact__title m-0 p-0">
-        Get in <span style="color:var(--color-blue)">Touch!</span>
-      </h4>
-      <p>We will answer as soon as we can!</p>
-      <div class="flex flex-row gap-2">
-        <input
-          class="input-1 p-3 bg-transparent border-solid border w-full"
-          placeholder="Full Name"
-        />
-        <input
-          class="input-1 p-3 bg-transparent border-solid border w-full"
-          placeholder="Email"
-        />
+  <div
+    class="flex flex-col justify-center items-center sm:col-span-6 md:col-span-9 lg:col-span-5 xl:col-span-6"
+  >
+    <div class="grid grid-row-8 gap-3">
+      <div class="flex flex-col justify-start items-start row-span-2">
+        <h4 class="font-bold m-0">
+          Get in <span style="color:var(--color-blue)">Touch!</span>
+        </h4>
+        <p class="font-base font-lightweight">
+          We will answer as soon as we can!
+        </p>
       </div>
-      <div class="flex">
+      <div class="flex justify-start items-center row-span-1">
+        <div class="grid grid-cols-4 gap-3">
+          <input
+            class="p-3 bg-transparent border-solid border col-span-2"
+            placeholder="Full Name"
+          />
+          <input
+            class="p-3 bg-transparent border-solid border col-span-2"
+            placeholder="Email"
+          />
+        </div>
+      </div>
+      <div class="flex justify-start items-center row-span-4 h-auto">
         <textarea
-          class="input-2 p-3 bg-transparent border-solid border w-full"
+          class="custom-scrollbar p-3 bg-transparent border-solid border w-full h-[40vh]"
           placeholder="Your Message"
         />
       </div>
-      <Button onClick={handleClick}>
-        <a href="../../ContactUs">Contact Us</a>
-      </Button>
+      <div class="flex row-span-1">
+        <Button onClick={handleClick}>
+          <a href="../../ContactUs">Contact Us</a>
+        </Button>
+      </div>
     </div>
   </div>
 </div>
 
 <style>
-  .contact__left {
-    max-width: 40%;
-    padding: 5rem;
-    padding-top: 3rem;
-  }
-  .contact__right {
-    max-width: 60%;
-    padding: 5rem;
-    padding-top: 3rem;
+  .custom-scrollbar::-webkit-scrollbar {
+    width: 8px;
   }
 
-  .contact__title {
-    margin-bottom: -20px;
-  }
-  .contact__right-container {
-    max-width: 75%;
+  .custom-scrollbar::-webkit-scrollbar-thumb {
+    background-color: var(--color-blue);
+    border-radius: 9999px;
   }
 
-  .input-2 {
-    max-width: inherit;
-    min-width: inherit;
-    height: 400px;
-  }
-
-  .input-1 {
-    max-width: inherit;
-    min-width: inherit;
+  .custom-scrollbar {
+    scrollbar-width: thin;
+    scrollbar-color: var(--color-blue) transparent;
   }
 </style>
