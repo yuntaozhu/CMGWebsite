f96dff8b315d5dd075e39160b3bdb15d022e2c51
Sean Joseph Marin <scmarin@up.edu.ph>
2023-08-31 20:39:24 +0800

    refactor: ChatBot | remove unused code

--- src/components/atoms/ChatBot/ChatBot.svelte
+++ src/components/atoms/ChatBot/ChatBot.svelte
@@ -1,19 +1,31 @@
 <script>
-    // @ts-nocheck
-
     import "./ChatBot.css"
     import LoadingDots from "./LoadingDots.svelte";
     import { fade, fly, scale } from "svelte/transition";
     import { cubicOut } from "svelte/easing"
     import { beforeUpdate, afterUpdate } from "svelte";
+    import AcssOrientation from "../../../routes/registration/form-acss-orientation/ACSSOrientation.svelte";
 
-    let isChatbotOpen = false;
-    let buttonTransitionDuration = 200;
+    /**
+     * @type {HTMLButtonElement}
+     */
     let chatbotButton;
-    let userInputValue = "";
+    /**
+     * @type {HTMLDivElement}
+     */
     let messagesContainer;
+    /**
+     * @type {HTMLTextAreaElement}
+     */
+    let userInput;
+    /**
+     * @type {HTMLButtonElement}
+     */
+    let sendButton;
+    let userInputValue = "";
+    let isChatbotOpen = false;
     let autoscroll = false;
-
+    let buttonTransitionDuration = 200;
     let messageHistory = [
         {
             isUser: false,
@@ -66,7 +78,7 @@
         }
     }
 
-    const fetchAnswer = async (question, userInput, sendButton) => {
+    const fetchAnswer = async (/** @type {string} */ question) => {
         // send question to api
         const res = await fetch("/api/chatbot", {
             method: "POST",
@@ -87,6 +99,7 @@
         
         // update message history
         messageHistory[messageHistory.length - 1] = {
+            ...messageHistory[messageHistory.length - 1],
             textLoaded: true,
             text: result
         }
@@ -105,9 +118,6 @@
         if (userInputValue == "") {
             return;
         }
-
-        let userInput = document.getElementById("user-input");
-        let sendButton = document.getElementById("submit-button");
 
         userInput.style.height = "16px";
         userInput.disabled = true;
@@ -135,11 +145,12 @@
                 text: "",
             }]
 
-            await fetchAnswer(question, userInput, sendButton)
+            await fetchAnswer(question)
         }, 500)
     }
 
     // resize textarea when typing long messages
+    // @ts-ignore
     const resizeTextarea = (e) => {
         e.target.style.height = "16px";
         e.target.style.height = e.target.scrollHeight + "px";
@@ -176,9 +187,9 @@
             <!-- input area -->
             <div class="flex py-4 px-5 gap-4 self-stretch border-t border-solid border-black h-fit box-border">
                 <!-- text area -->
-                <textarea id="user-input" class="flex self-stretch resize-none flex-[1_0_0] bg-transparent border-none outline-0 h-4 max-h-14 text-xs" placeholder="Type your message..." bind:value={userInputValue} on:input={resizeTextarea}/>
+                <textarea bind:this={userInput} class="flex self-stretch resize-none flex-[1_0_0] bg-transparent border-none outline-0 h-4 max-h-14 text-xs" placeholder="Type your message..." bind:value={userInputValue} on:input={resizeTextarea}/>
                 <!-- send button -->
-                <button id="submit-button" class="disabled:opacity-50" on:click={sendQuestion}>
+                <button bind:this={sendButton} class="disabled:opacity-50" on:click={sendQuestion}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                         <g clip-path="url(#clip0_889_678)">
                             <path d="M19.4565 0.218381C19.851 0.491818 20.058 0.964474 19.9838 1.43713L17.4838 17.6871C17.4252 18.066 17.1947 18.3981 16.8588 18.5856C16.5229 18.7731 16.1205 18.7965 15.7651 18.6481L11.0932 16.7067L8.4174 19.6012C8.06974 19.9801 7.52287 20.1051 7.0424 19.9176C6.56193 19.7301 6.24943 19.2653 6.24943 18.7496V15.484C6.24943 15.3278 6.30802 15.1793 6.41349 15.066L12.9604 7.92151C13.1869 7.67541 13.1791 7.29651 12.9447 7.06213C12.7104 6.82776 12.3315 6.81213 12.0854 7.03479L4.14005 14.0934L0.690837 12.3668C0.276774 12.1598 0.011149 11.7457 -0.000569705 11.2848C-0.0122885 10.8238 0.229899 10.3942 0.628336 10.1637L18.1283 0.163693C18.5463 -0.0745882 19.0619 -0.0511507 19.4565 0.218381Z" fill="#F4F1FF"/>
--- src/routes/api/chatbot/+server.js
+++ src/routes/api/chatbot/+server.js
@@ -1,6 +1,5 @@
 import { client } from "@gradio/client";
 import { error, json } from "@sveltejs/kit";
-import { env } from "$env/dynamic/private";
 
 export async function POST ({ request }) {
     // extract question
@@ -9,21 +8,6 @@ export async function POST ({ request }) {
         const app = await client("https://sejo-acss-chatbot.hf.space/", {});
         const result = await app.predict("/predict", [question]);
 
-        // fetch answer from Flask server
-        // const res = await fetch("http://127.0.0.1:5000/chatbot", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     },
-        //     body: JSON.stringify({ question })
-        // });
-        // const result = await res.json();
-
-        // return new Promise((resolve) => {
-        //     setTimeout(() => {
-        //         resolve(json(result))
-        //     }, 1000)
-        // })
         // @ts-ignore
         return json({result: result.data[0]})
     } catch {
