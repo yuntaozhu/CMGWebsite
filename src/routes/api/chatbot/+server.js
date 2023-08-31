49dc2dafdbf4fb1d31983d76e7dbc0ed2043e18e
Sean Joseph Marin <scmarin@up.edu.ph>
2023-08-31 19:41:36 +0800

    fix: ChatBot | Connected the chatbot to the online ML chatbot app

--- src/routes/api/chatbot/+server.js
+++ src/routes/api/chatbot/+server.js
@@ -1,26 +1,31 @@
+import { client } from "@gradio/client";
 import { error, json } from "@sveltejs/kit";
+import { env } from "$env/dynamic/private";
 
 export async function POST ({ request }) {
     // extract question
     let { question } = await request.json()
     try {
+        const app = await client("https://sejo-acss-chatbot.hf.space/", {});
+        const result = await app.predict("/predict", [question]);
+
         // fetch answer from Flask server
-        const res = await fetch("http://127.0.0.1:5000/chatbot", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ question })
-        });
-        const result = await res.json();
+        // const res = await fetch("http://127.0.0.1:5000/chatbot", {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify({ question })
+        // });
+        // const result = await res.json();
 
         // return new Promise((resolve) => {
         //     setTimeout(() => {
         //         resolve(json(result))
         //     }, 1000)
         // })
-
-        return json(result)
+        // @ts-ignore
+        return json({result: result.data[0]})
     } catch {
         throw error(500, "Sorry, there was an error with the server. Please try again later.");
     }
