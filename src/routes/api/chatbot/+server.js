18c7b55b61b02a47336b4256bd8016aadb85f1da
Sean Joseph Marin <scmarin@up.edu.ph>
2023-09-03 00:18:53 +0800

    fix: ChatBot | remove connection to chatbot api

--- src/routes/api/chatbot/+server.js
+++ src/routes/api/chatbot/+server.js
@@ -5,12 +5,6 @@ export async function POST ({ request }) {
     let { question } = await request.json()
     try {
         throw error(500, "Sorry, there was an error with the server. Please try again later.");
-
-        // const app = await client("https://sejo-acss-chatbot.hf.space/", {});
-
-        // const result = await app.predict("/predict", [question]);
-
-        // return json({result: result.data[0]})
     } catch {
         throw error(500, "Sorry, there was an error with the server. Please try again later.");
     }
