ded7c374c1834eae5a98a1f7e7a58a6a4e2f27fa
Sean Joseph Marin <scmarin@up.edu.ph>
2023-09-03 16:43:19 +0800

    feat: ChatBot | Connect ChatBot to ML api

--- src/routes/api/chatbot/+server.js
+++ src/routes/api/chatbot/+server.js
@@ -1,26 +1,27 @@
 import { error, json } from "@sveltejs/kit";
+import { client } from "@gradio/client";
 
 export async function POST ({ request }) {
     // extract question
     let { question } = await request.json()
     try {
+        const app = await client("https://sejo-acss-chatbot.hf.space/", {});
+        /**
+         * @type {any}
+         */
+        const result = await app.predict("/predict", [question]);
+
         // fetch answer from Flask server
-        const res = await fetch("http://127.0.0.1:5000/chatbot", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ question })
-        });
-        const result = await res.json();
+        // const res = await fetch("http://127.0.0.1:5000/chatbot", {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify({ question })
+        // });
+        // const result = await res.json();
 
-        // return new Promise((resolve) => {
-        //     setTimeout(() => {
-        //         resolve(json(result))
-        //     }, 1000)
-        // })
-
-        return json(result)
+        return json({result: result.data[0]})
     } catch {
         throw error(500, "Sorry, there was an error with the server. Please try again later.");
     }
