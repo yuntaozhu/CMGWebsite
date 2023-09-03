ae450e8e0242e268252cc4e9761f7c027049de83
Sean Joseph Marin <scmarin@up.edu.ph>
2023-09-03 17:11:31 +0800

    docs: ChatBot | added documentation

--- src/routes/api/chatbot/+server.js
+++ src/routes/api/chatbot/+server.js
@@ -5,21 +5,14 @@ export async function POST ({ request }) {
     // extract question
     let { question } = await request.json()
     try {
+        // connect to HuggingFace Spaces
         const app = await client("https://sejo-acss-chatbot.hf.space/", {});
+        
+        // make a prediction (ask a question to the chatbot)
         /**
          * @type {any}
          */
         const result = await app.predict("/predict", [question]);
-
-        // fetch answer from Flask server
-        // const res = await fetch("http://127.0.0.1:5000/chatbot", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     },
-        //     body: JSON.stringify({ question })
-        // });
-        // const result = await res.json();
 
         return json({result: result.data[0]})
     } catch {
