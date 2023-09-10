197a31967e2d3aef133d18690fc4a62f3f0faa88
eslomat <eslomat@up.edu.ph>
2023-09-10 20:32:57 +0800

    fix: Showcase | Fixed showcase bug

--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { onMount } from "svelte";
+  // @ts-nocheck
+
   import {
     Activities,
     Events,
@@ -9,6 +10,8 @@
     ShowcaseTitle,
     ShowcaseCards,
   } from "$components";
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
 
   const showcaseComponents = {
     Projects,
@@ -18,7 +21,20 @@
   };
 
   let tabs = ["Projects", "Events", "Workshops", "Activities"];
-  let activeTab = "Projects"; // Default active tab
+  let activeTab;
+  let show = false;
+
+  try {
+    activeTab = sessionStorage.getItem("activeTab");
+    console.log("im here");
+  } catch {
+    activeTab =
+      $page.url.hash === ""
+        ? "Projects"
+        : `${$page.url.hash[1].toUpperCase()}${$page.url.hash
+            .toLowerCase()
+            .substr(2, $page.url.hash.length - 1)}`;
+  }
 
   const tabChange = (e) => {
     activeTab = e.detail;
@@ -26,36 +42,53 @@
   };
 
   function scrollTabChange() {
-    if (typeof window !== "undefined") {
-      activeTab = sessionStorage.getItem("activeTab") || "Projects";
-    }
+    activeTab = sessionStorage.getItem("activeTab") || "Projects";
   }
 
   onMount(() => {
-    if (typeof window !== "undefined") {
-      // Check if running on the client side
-      // Reset activeTab to "Projects" on page reload
-      sessionStorage.setItem("activeTab", activeTab);
-      window.addEventListener("scrollend", scrollTabChange);
-      return () => {
-        removeEventListener("scrollend", scrollTabChange);
-      };
+    if ($page.url.hash !== "") {
+      activeTab = `${$page.url.hash[1].toUpperCase()}${$page.url.hash
+        .toLowerCase()
+        .substr(2, $page.url.hash.length - 1)}`;
+      sessionStorage.setItem(
+        "activeTab",
+        `${$page.url.hash[1].toUpperCase()}${$page.url.hash
+          .toLowerCase()
+          .substr(2, $page.url.hash.length - 1)}`
+      );
     }
+    if(activeTab === null){
+      activeTab = "Projects";
+    }
+    show = true;
+    console.log(activeTab);
+    window.addEventListener("scrollend", scrollTabChange);
+    return () => {
+      removeEventListener("scrollend", scrollTabChange);
+    };
   });
 </script>
 
 <head>
-  <title>Showcasing Excellence - The Alliance of Computer Science Students UPLB</title>
-  <meta name="title" content="Showcasing Excellence - The Alliance of Computer Science Students UPLB" />
+  <title
+    >Showcasing Excellence - The Alliance of Computer Science Students UPLB</title
+  >
   <meta
-  property="og:title"
-  content="Showcasing Excellence - The Alliance of Computer Science Students UPLB"
+    name="title"
+    content="Showcasing Excellence - The Alliance of Computer Science Students UPLB"
   />
   <meta
-  property="twitter:title"
-  content="Showcasing Excellence - The Alliance of Computer Science Students UPLB"
+    property="og:title"
+    content="Showcasing Excellence - The Alliance of Computer Science Students UPLB"
   />
-  <meta name="description" content="Explore our diverse portfolio of innovative projects, proudly presented by The Alliance of Computer Science Students - UPLB. Witness the future of technology and creativity at its best." />
+  <meta
+    property="twitter:title"
+    content="Showcasing Excellence - The Alliance of Computer Science Students UPLB"
+  />
+  <meta
+    name="description"
+    content="Explore our diverse portfolio of innovative projects, proudly presented by The Alliance of Computer Science Students - UPLB. Witness the future of technology and creativity at its best."
+  />
   <meta
     property="og:description"
     content="Explore our diverse portfolio of innovative projects, proudly presented by The Alliance of Computer Science Students - UPLB. Witness the future of technology and creativity at its best."
@@ -66,20 +99,17 @@
   />
 </head>
 
-<div id={activeTab.toLocaleLowerCase()} class="overflow-hidden w-full flex flex-col items-center px-5 py-10 relative z-20" >
+<div
+  class="overflow-hidden w-full flex flex-col items-center px-5 py-10 relative z-20 min-h-screen"
+>
+  <ShowcaseTitle />
 
-  <ShowcaseTitle/>
-  
-  <ShowcaseNavBar {activeTab} {tabs} on:tabChange={tabChange}/>
-
-
-  {#each tabs as tab}
-
-    {#if activeTab === tab}
-      <svelte:component this={showcaseComponents[tab]}/>
-    {/if}
-  {/each}
-
+  {#if show}
+    <ShowcaseNavBar {activeTab} {tabs} on:tabChange={tabChange} />
+    {#each tabs as tab}
+      {#if activeTab === tab}
+        <svelte:component this={showcaseComponents[tab]} />
+      {/if}
+    {/each}
+  {/if}
 </div>
-
-
