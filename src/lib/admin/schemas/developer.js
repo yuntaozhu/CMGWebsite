1c3224652412e3d2c42299b099a960172b4b0117
Sean Joseph Marin <scmarin@up.edu.ph>
2024-01-31 12:23:12 +0800

    Developers | fix: change Sanity developer schema

--- src/lib/admin/schemas/developer.js
+++ src/lib/admin/schemas/developer.js
@@ -1,4 +1,4 @@
-import {defineField, defineType} from 'sanity'
+import {defineArrayMember, defineField, defineType} from 'sanity'
 
 export default defineType({
   name: 'developer',
@@ -6,45 +6,95 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-    }),
-    defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'name',
-        maxLength: 96,
-      },
-    }),
-    defineField({
       name: 'image',
       title: 'Profile Picture',
       type: 'image',
       options: {
         hotspot: true,
       },
+      description: "Kindly add a profile picture to introduce the developer's face to the world."
     }),
     defineField({
-      name: 'bio',
-      title: 'Bio',
-      type: 'array',
-      of: [
-        {
-          title: 'Block',
-          type: 'block',
-          styles: [{title: 'Normal', value: 'normal'}],
-          lists: [],
-        },
-      ],
+      name: "firstName",
+      title: "First Name",
+      type: "string",
+      description: "Kindly add a first name for the developer."
     }),
+    defineField({
+      name: "middleName",
+      title: "Middle Name",
+      type: "string"
+    }),
+    defineField({
+      name: "lastName",
+      title: "Last Name",
+      type: "string",
+      description: "Kindly add a last name for the developer."
+    }),
+    defineField({
+      name: "suffix",
+      title: "Suffix",
+      type: "string"
+    }),
+    defineField({
+      name: "batch",
+      title: "Organization Batch",
+      type: "string",
+      description: "Kindly indicate the batch of the developer."
+    }),
+    defineField({
+      name: "email",
+      title: "Email",
+      type: "string",
+      description: "This field is not required, but kindly indicate an email, so that others can contact this developer."
+    }),
+    defineField({
+      name: "bio",
+      title: "Personal Bio",
+      type: "string",
+      description: "Kindly add a Personal Bio to tell more about this developer."
+    }),
+    defineField({
+      name: "linkedin",
+      title: "LinkedIn Profile Link",
+      type: "string",
+      description: "This field is not required, but kindly indicate a LinkedIn Profile so that other people can connect with this developer."
+    }),
+    defineField({
+      name: "github",
+      title: "GitHub Profile Link",
+      type: "string",
+      description: "This field is not required, but kindly indicate a GitHub Profile so that other people can check the projects made by this developer."
+    }),
+    defineField({
+      name: "skills",
+      title: "Team",
+      type: "array",
+      of: [
+        defineArrayMember({
+          type: "string"
+        })
+      ]
+    }),
+    defineField({
+      name: "team",
+      title: "Team",
+      type: "reference",
+      to: [{ type: "team"}],
+      description: "Kindly indicate the team of the developer."
+    })
   ],
   preview: {
     select: {
-      title: 'name',
       media: 'image',
+      firstName: "firstName",
+      lastName: "lastName",
     },
+    prepare: ({media, firstName, lastName}) => {
+      return {
+        title: `${firstName} ${lastName}`,
+        media
+      }
+    }
   },
 })
